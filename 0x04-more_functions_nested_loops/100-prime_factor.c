86da37adee58f6642858218ab3d855b9c939cf98
adele_aboaba <bidemidone@gmail.com>
2023-03-26 02:55:18 -0700

    solved

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -21,7 +21,7 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max = 2;
-		n / = 2;
+		n = n / 2;
 	}
 
 	for (i = 3; i <= sqrt(n); i = i + 2)
@@ -36,7 +36,7 @@ int main(void)
 	if (n > 2)
 		max = n;
 
-	printf("%Id\n", max);
+	printf("%ld\n", max);
 
 	return (0);
 }
