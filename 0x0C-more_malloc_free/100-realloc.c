d8e599f7de4d98dcb3181291ee2782be5166c903
adele_aboaba <bidemidone@gmail.com>
2023-04-16 15:21:06 -0700

    done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -42,7 +42,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr[i] = old_ptr[i];
+			ptr1[i] = old_ptr[i];
 	}
 
 	free(ptr);
